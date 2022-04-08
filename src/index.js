911824ecb12068725e0004cdd24d3c81c8a1ade6
Jorge Roldán <icabina@live.com>
2022-04-08 11:00:47 -0500

    m

---
+++ src/index.js
@@ -0,0 +1,49 @@
+//TODO
+//npm install apollo-server graphql
+// npm install apollo-datasource-rest
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
+
+/*  With mocks enabled, Apollo Server always returns exactly two entries for every list field.
+    To get more entries at a time, let's say 6, we'll add a Query.tracksForHome to our mocks object and return an Array of that given length like so: [...new Array(6)]. */
+/* const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+}; */
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
+  /* mocks */
+});
+
+server.listen().then(() => {
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at https://studio.apollographql.com/dev
+    `);
+});
